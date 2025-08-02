4776923466b9d136abc0a960c4b3116b8220ec9a
MyWayOrHightWay <nfz8723@gmail.com>
2025-08-02 17:41:34 +0500

    container and font have been added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<>
+			<header className='container h-[80px] flex items-center justify-between bg-gray-800 text-white'>
+				<h1 className='font-[orbitron] text-[22px] font-[700] cursor-pointer'>
+					MetaV
+				</h1>
+				<nav>
+					<ul className='flex items-center gap-4 font-[poppins] text-[16px] font-[400] cursor-pointer'>
+						<li>Home</li>
+						<li>Resource</li>
+						<li>Pricing</li>
+						<li>About us</li>
+					</ul>
+				</nav>
+			</header>
+			<section className='container bg-gray-800 text-white'>
+				<div>
+					<h1 className='font-[orbitron] text-[45px] font-[600]'>
+						Explore the future of the
+						<span className='text-[55px] font-[700] bg-gradient-to-r from-[#F86CF8] to-[#64CEF5] bg-clip-text text-transparent'>
+							Metaverse
+						</span>
+					</h1>
+				</div>
+			</section>
+		</>
+	)
 }
 
 export default Home
