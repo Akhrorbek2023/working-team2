c823b58c0e22f56d1f8ad6e1447f55a0e6fa2f80
omina08 <faxriddinovaomina8@email.com>
2025-08-02 17:58:30 +0500

    add pic

---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+<div>
+<div className='flex bg-[#090823] text-white justify-between p-12'>
+      <div>
+        <h1 className='text-2xl font-semibold mb-4 text-purple-400'>Meta V</h1>
+        <p>Adipiscing at in tellus integer. Pellentesque massa <br /> placerat duis ultricies lacus. Nisi porta lorem mollis <br /> aliquam ut porttitor</p>
+      </div>
+
+      {/* About us */}
+
+      <div> 
+        <ul>
+            <li className='text-2xl font-semibold mb-4'>About Us</li>
+            <li>About MetaV</li>
+            <li>Contact Us</li>
+            <li>FAQ</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+        </ul>
+      </div>
+
+      {/* Sevices */}
+
+      <div> 
+        <ul>
+            <li className='text-2xl font-semibold mb-4'>NFT Marketplace</li>
+            <li>Virtual Concert</li>
+            <li>Gaming</li>
+            <li>Artwork</li>
+            <li>Online Travel</li>
+        </ul>
+      </div>
+
+      {/* Contact */}
+
+      <div>
+        <div className='flex gap-[10px] mb-4' >
+            <img src="/LinkedIn.png" alt="" />
+            <img src="/Twitter Squared.png" alt="" />
+            <img src="/Instagram.png" alt="" />
+            <img src="/Facebook.png" alt="" />
+        </div>
+        <h1>2022 © MetaV.com All right <br /> reserved.</h1>
+      </div>
+
+    </div>
+</div>
+  )
+}
+
+export default Footer
